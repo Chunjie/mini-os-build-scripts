7659daacbd91b7a683cb5083b1ab4610518e3a83
Chunjie Zhu <chunjie.zhu@xxx.com>
2015-12-21 09:44:22 -0500

    fix the path issue in build script

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+CWD=`pwd`
+
 #prepare
 [ -d /local/build ] || mkdir -p /local/build
 cd /local/build
@@ -13,8 +15,8 @@ git clone git@github.com:Chunjie/lwip-for-minios.git -b 1.3.2
 
 #build net app_main (without libc)
 cd /local/build/mini-os
-git apply mini-os-patch/0001-add-a-global-system-monitor-thread.patch
-git apply mini-os-patch/0002-set-a-global-ip-address.patch
+git apply $CWD/mini-os-patch/0001-add-a-global-system-monitor-thread.patch
+git apply $CWD/mini-os-patch/0002-set-a-global-ip-address.patch
 make LWIPDIR=/local/build/lwip-for-minios
 
 #build main (with libc)
