fbc954bc05565a7535100e4ada2b311ab6bb90f3
Chunjie Zhu <chunjie.zhu@xxx.com>
2015-12-21 10:01:32 -0500

    save mini-os source code into github my own repo

--- build.sh
+++ build.sh
@@ -7,14 +7,15 @@ CWD=`pwd`
 #prepare
 [ -d /local/build ] || mkdir -p /local/build
 cd /local/build
-git clone git://xenbits.xen.org/mini-os.git
+#git clone git://xenbits.xen.org/mini-os.git
+git clone git@github.com:Chunjie/mini-os-unikernel.git -b 2015-12-03
 
 #wget http://download.savannah.gnu.org/releases/lwip/older_versions/lwip-1.3.2.zip
 #unzip lwip-1.3.2.zip
 git clone git@github.com:Chunjie/lwip-for-minios.git -b 1.3.2
 
 #build net app_main (without libc)
-cd /local/build/mini-os
+cd /local/build/mini-os-unikernel
 git apply $CWD/mini-os-patch/0001-add-a-global-system-monitor-thread.patch
 git apply $CWD/mini-os-patch/0002-set-a-global-ip-address.patch
 make LWIPDIR=/local/build/lwip-for-minios
