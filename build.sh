38249188d1c15a4d612851bf6294846ef5f9ab34
Chunjie Zhu <chunjie.zhu@xxx.com>
2015-12-24 11:39:18 -0500

    add a patch to support key handler

--- build.sh
+++ build.sh
@@ -18,6 +18,7 @@ git clone git@github.com:Chunjie/lwip-for-minios.git -b 1.3.2
 cd /local/build/mini-os-unikernel
 git apply $CWD/mini-os-patch/0001-add-a-global-system-monitor-thread.patch
 git apply $CWD/mini-os-patch/0002-set-a-global-ip-address.patch
+git apply $CWD/mini-os-patch/0003-add-key-handler-to-display-system-status.patch
 make LWIPDIR=/local/build/lwip-for-minios
 
 #build main (with libc)
